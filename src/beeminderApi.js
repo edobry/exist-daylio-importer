e3b4521f0f7d9fbffb31cc57cb86a2c1d8b65d1a
Eugene Dobry <eugene@dobry.me>
2020-02-11 00:44:36 -0500

    reimplemented api requests

--- src/beeminderApi.js
+++ src/beeminderApi.js
@@ -7,37 +7,68 @@ nconf.argv().file("conf.json");
 const
     { log, logJSON } = require("./util");
 
-const bm = beeminder(nconf.get("beeminder:auth_token"));
+const { username, auth_token } = nconf.get("beeminder");
+
+const baseUrl = "https://www.beeminder.com/api/v1";
+
+const goalEndpoint = "goals"
 
 const daylioGoalName = "daylio-checkins";
+const daylioGoalUrl = `users/${username}/${goalEndpoint}/${daylioGoalName}`;
+
+const bmApi = got.extend({
+    prefixUrl: baseUrl,
+    searchParams: { auth_token },
+    responseType: 'json'
+});
 
 const getDaylioGoal = async () => {
-    const response = await bm.getGoal(daylioGoalName);
-    logJSON(response);
+    try {
+        const response = await bmApi({
+            method: "GET",
+            url: `${daylioGoalUrl}.json`
+        });
+
+        log(response.body);
+    } catch(e) {
+        log(e);
+        // log(e.response);
+        // log(e.options);
+    }
 };
 
 const logDaylioSync = async () => {
     log("Logging to Beeminder...");
 
+    const vals = {
+        value: 1,
+        comment: "logged from exist-daylio-importer",
+        sendmail: true,
+        requestid: new Date().toLocaleDateString()
+    };
+
     try {
-        const response = await bm.createDatapoint(daylioGoalName, {
-            value: 1,
-            comment: "logged from exist-daylio-importer",
-            sendmail: true,
-            requestid: new Date().toLocaleDateString()
+        const response = await bmApi({
+            method: "POST",
+            url: `${daylioGoalUrl}/datapoints.json`,
+            json: vals
         });
 
-        const { id, status } = response;
+        const { id, status } = response.body;
 
         log(`Beeminder datapoint '${id}' ${status}`);
 
         return response;
     } catch(e) {
-        if(e == "Duplicate request")
+        const { errors } = e.response.body;
+
+        if(errors == "Duplicate request") {
+            log("Duplicate request, exiting");
             return;
+        }
 
         else {
-            log(e);
+            log(errors);
             throw new Error("Unknown error!");
         }
     }
